985c5f08c81f7d0dcf4ed83c79fef4bc1170b264
Brian Collura <bjscollura@gmail.com>
2023-06-21 18:31:38 -0400

    Updates to old crumby code, moving styles to components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
     import { browser } from "$app/environment";
     import { page } from "$app/stores";
-    export const prerender = true;
+    // export const prerender = true;
 
     let isYScroll = false;
     if (browser) {
@@ -13,27 +13,27 @@
             }
         }
     }
-    console.log($page)
+    console.log("page is: ", $page)
 </script>
 
 <div class="header-container container {isYScroll ? 'header-shadow' : ''}">
     <header>
         <div class="logo"><a href="/" style="color:#e4ebe4;">Brian Collura</a></div>
         <ul class="header-nav">
-        <li><a href="/" class:nav-here={$page.route.id === '/'}>Work</a></li>
-        <li><a href="/about" class:nav-here={$page.route.id === '/about'}>About</a></li>
+          <li><a href="/" class:nav-here={$page.route.id === '/'}>Work</a></li>
+          <li><a href="/about" class:nav-here={$page.route.id === '/about'}>About</a></li>
         </ul>
         <ul class="header-links">
-        <li><a href="http://linkedin.com/in/bjscollura" class="icon-link" target="_blank">
-            <i class="fab fa-linkedin-in fa-lg"></i></a>
-        </li>
-        <li><a href="https://github.com/bjscollura/" class="icon-link" target="_blank">
-            <i class="fab fa-github fa-lg"></i></a>
-        </li>
-        <li>
-            <a href="/Resume_June2023.pdf" target="_blank" class="button">
-            <div>Resume.pdf</div></a>
-        </li>
+          <li><a href="http://linkedin.com/in/bjscollura" class="icon-link" target="_blank">
+              <i class="fab fa-linkedin-in fa-lg"></i></a>
+          </li>
+          <li><a href="https://github.com/bjscollura/" class="icon-link" target="_blank">
+              <i class="fab fa-github fa-lg"></i></a>
+          </li>
+          <li>
+              <a href="/Resume_June2023.pdf" target="_blank" class="button">
+              <div>Resume.pdf</div></a>
+          </li>
         </ul>
     </header>
 </div>
@@ -59,81 +59,101 @@
 </div>
 
 <style>
-    .header-container {
-        justify-content: center;
-        background-color: #262620;
-        z-index: 2;
-        width: 100%;
-        position: fixed;
-        top: 0;
-        left: 0;
-        padding: 20px 50px;
-        transition: box-shadow .5s ease-in;
-    }
-    .header-shadow {
-        box-shadow: 1px 1px .5em black;
-
-    }
+  /* Header */
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    max-width: 1899px;
+    height: 80px;
+  }
+  .header-container {
+    justify-content: center;
+    background-color: #262620;
+    z-index: 2;
+    width: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding: 20px 50px;
+    transition: box-shadow .5s ease-in;
+  }
+  .header-shadow {
+    box-shadow: 1px 1px .5em black;
+  }
+  .header-nav {
+    display: flex;
+    justify-content: center;
+    width: 33.3%;
+    list-style: none;
+  }
+  .header-nav > li {
+    margin-right: 2rem;
+  }
+  .header-nav > li:last-of-type {
+    margin-right: 0;
+  }
+  .header-nav a {
+    color: #e4ebe4;
+    font-size: 1.1rem;
+    padding: 10px 0;
+  }
+  .nav-here {
+    border-bottom: 2px solid #e4ebe4;
+  }
+  .header-links {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    width: 33.3%;
+    list-style: none;
+  }
+  .header-links > li {
+    margin-right: 1rem;
+  }
+  .header-links > li:last-of-type {
+    margin-right: 0;
+  }
+  .header-links a {
+    padding: 10px;
+  }
+  @media only screen and (max-width: 767px) {
     header {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        width: 100%;
-        height: 80px;
+      flex-direction: column;
+      height: auto;
     }
-    
-.header-nav {
-  display: flex;
-  justify-content: center;
-  width: 33.3%;
-  list-style: none;
-}
-.header-nav > li {
-  margin-right: 2rem;
-}
-.header-nav > li:last-of-type {
-  margin-right: 0;
-}
-.header-nav a {
-  color: #e4ebe4;
-  font-size: 1.1rem;
-  padding: 10px 0;
-}
-.nav-here {
-  border-bottom: 2px solid #e4ebe4;
-}
-
-.header-links {
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  width: 33.3%;
-  list-style: none;
-}
-.header-links > li {
-  margin-right: 1rem;
-}
-.header-links > li:last-of-type {
-  margin-right: 0;
-}
-.header-links a {
-  padding: 10px;
-}
-/* Footer */
-.footer-container {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-}
-.footer-links a {
-  margin: 0 .5rem;
-  padding: 10px;
-}
-.footer-container p {
-  text-align: center;
-}
-.footer-container hr {
-  margin-bottom: 50px;
-}
+    .logo {
+      width: 90%;
+      text-align: center;
+      margin: .5rem auto .0 auto;
+    }
+    .header-nav {
+      width: 90%;
+      margin: .7rem auto;
+    }
+    .header-links {
+      justify-content: center;
+      align-items: center;
+      width: 90%;
+      margin: 1rem auto 0 auto;
+    }
+  }
+  /* Footer */
+  .footer-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+  }
+  .footer-links a {
+    margin: 0 .5rem;
+    padding: 10px;
+  }
+  .footer-container p {
+    text-align: center;
+  }
+  .footer-container hr {
+    margin-bottom: 50px;
+  }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,56 +1,54 @@
 
 <div class="page-container container">
-    <div class="hero">
-      <div class="hero-heading">
-        <h1 class="h1-hero">I'm <strong>Brian</strong>.</h1>
-        <h1 class="h1-hero">I speak <strong>person</strong> and <strong>code</strong>.</h1>
-      </div>
-      <div class="hero-p">
-        <h6>I'm a User Experience Designer, a Web Developer, and a fellow citizen recovering from 2020.</h6>
-        <h6>Our digital future is going to be anxious and exciting.</h6>
-        <h6>I'd like to help keep it honest and lovely.</h6>
-      </div>
-      <div class="hero-button">
-        <a href="/about" class="button"><div>More about me</div></a>
-      </div>
-    </div>
+		<div class="hero">
+			<div class="hero-heading">
+				<h1 class="h1-hero">I'm <strong>Brian</strong>.</h1>
+				<h1 class="h1-hero">I speak <strong>person</strong> and <strong>code</strong>.</h1>
+			</div>
+			<div class="hero-p">
+				<h6>I'm a User Experience Designer, a Web Developer, and a fellow person in this modern world.</h6>
+				<h6>Our digital future is going to be anxious and exciting.</h6>
+				<h6>I'd like to help keep it honest and lovely.</h6>
+			</div>
+			<div class="hero-button">
+				<a href="/about" class="button"><div>More about me</div></a>
+			</div>
+		</div>
 
-    <div style="margin: 100px auto;"></div>
+		<div class="study-container container">
+			<h1>UX Design Work</h1>
+			
+			<div class="study">
+				<h2>Long Form Case Study: <br/>Designing an Amazon Alexa Skill</h2>
+				<figure>
+					<img src="/images/Alexa_2.jpg" alt="An Alexa Echo device" style="max-width: 1000px;" />
+					<figcaption></figcaption>
+				</figure>
+				<h3>Exploring New Voice Technology for an Old City</h3>
+				<div class="study-summary">
+					<p>Voice Assistants are still <strong>new</strong></p>
+					<p>and <strong>a little buggy</strong>.</p>
+					<p>We researched <strong>Amazon's Alexa</strong></p>
+					<p>to give a new voice to <strong>the City of Boston</strong>.</p>
+				</div>
+				<a href="/projects/cob" class="button"><div>Learn more</div></a>
+			</div>
 
-    <div class="study-container container">
-      <h1>UX Design Work</h1>
-      
-      <div class="study">
-        <h2>Long Form Case Study: <br/>Designing an Amazon Alexa Skill</h2>
-        <figure>
-          <img src="/images/Alexa_2.jpg" alt="An Alexa Echo device" style="max-width: 1000px;" />
-          <figcaption></figcaption>
-        </figure>
-        <h3>Exploring New Voice Technology for an Old City</h3>
-        <div class="study-summary">
-          <p>Voice Assistants are still <strong>new</strong></p>
-          <p>and <strong>a little buggy</strong>.</p>
-          <p>We researched <strong>Amazon's Alexa</strong></p>
-          <p>to give a new voice to <strong>the City of Boston</strong>.</p>
-        </div>
-        <a href="/projects/cob" class="button"><div>Learn more</div></a>
-      </div>
+			<div class="study">
+				<h2>Short Form Case Study: <br/>Designing "Search"</h2>
+				<figure>
+					<img src="/images/MindBody_2.png" alt="The MindBody logo" style="max-width: 1000px;" />
+					<figcaption></figcaption>
+				</figure>
+				<h3>When Features and Branding Feel “Clunky”</h3>
+				<div class="study-summary">
+					<p>MindBody is an <strong>app for booking health and fitness classes</strong>...</p>
+					<p>...But also haircuts and doctor's appointments.</p>
+					<p>I worked in a team of 3 to <strong>identify the real needs of the users</strong>,</p>
+					<p>and <strong>streamline the search and results</strong> pages.</p>
+				</div>
+				<a href="/projects/mindbody" class="button"><div>Learn more</div></a>
+			</div>
 
-      <div class="study">
-        <h2>Short Form Case Study: <br/>Designing "Search"</h2>
-        <figure>
-          <img src="/images/MindBody_2.png" alt="The MindBody logo" style="max-width: 1000px;" />
-          <figcaption></figcaption>
-        </figure>
-        <h3>When Features and Branding Feel “Clunky”</h3>
-        <div class="study-summary">
-          <p>MindBody is an <strong>app for booking health and fitness classes</strong>...</p>
-          <p>...But also haircuts and doctor's appointments.</p>
-          <p>I worked in a team of 3 to <strong>identify the real needs of the users</strong>,</p>
-          <p>and <strong>streamline the search and results</strong> pages.</p>
-        </div>
-        <a href="/projects/mindbody" class="button"><div>Learn more</div></a>
-      </div>
-
-    </div>
+		</div>
 </div>
